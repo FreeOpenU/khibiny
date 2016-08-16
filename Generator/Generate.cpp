6e8a236b3d86dfe8b392ef9f5cd663a11b7dff34
Atrytone <the.atrytone@gmail.com>
2016-08-16 01:56:48 -0700

    Added more finesse to the information generated
    Added:
    Account Number function
    Checking and Savings function
    
    To Add:
    Name of Account Holder function
    And wrap the generator in a class

--- Generator/Generate.cpp
+++ Generator/Generate.cpp
@@ -1,7 +1,54 @@
 #include "Generate.h"
 
 
+std::string generate_holder(std::minstd_rand0 &generator, std::uniform_int_distribution<int> index) {
+  std::string full_name = "";
 
+  return full_name;
+}
+
+std::string generate_acc(std::minstd_rand0 &generator, std::uniform_int_distribution<int> digits) {
+  std::string account_num = "Account Number: ";
+
+  for (int i = 0; i < 10; i++) { //The Typical bank account has 9 or 10 digits to safely hold its member count
+    int num = digits(generator);
+    account_num += std::to_string(num);
+  }
+  return account_num;
+}
+
+std::string generate_money(std::minstd_rand0 & generator,
+                          std::uniform_int_distribution<int> size) {
+
+  std::string full_string = "";
+
+  std::string checking = "Checking: $";
+  std::string check_amount;
+
+  std::string savings = "Savings: $";
+  std::string savings_amount;
+
+  std::uniform_int_distribution<int> change(10,99);
+
+  int change_check = change(generator);
+  int change_savings = change(generator);
+
+  int check_amount_int = digits(generator);
+  int savings_amount_int = digits(generator);
+
+  checking += std::to_string(check_amount_int);
+  checking += ".";
+  checking += std::to_string(change_check);
+
+  savings += std::to_string(savings_amount_int);
+  savings += ".";
+  savings += std::to_string(change_savings);
+
+  full_string = checking + " " + savings;
+
+  return full_string;
+
+}
 
 std::string generate_cc(std::minstd_rand0 & generator,
                         std::uniform_int_distribution<int> digits) {
@@ -19,7 +66,7 @@ std::string generate_cc(std::minstd_rand0 & generator,
 }
 
 std::string generate_exp(std::minstd_rand0 & generator,
-                         std::uniform_int_distribution<int> digits) {
+                         std::uniform_int_distribution<int> months) {
   std::string expiration = "EXP: ";
   std::string month;
   std::string year;
--- Generator/main.cpp
+++ Generator/main.cpp
@@ -8,18 +8,23 @@ int main () {
   time_t timer;
   unsigned seed = time(&timer);
   std::minstd_rand0 generator(seed);
-  std::uniform_int_distribution<int> digits(0,9);
-  std::uniform_int_distribution<int> months(0,12);
+  std::uniform_int_distribution<int> digits(0,9); //Range to select from for CC numbers
+  std::uniform_int_distribution<int> months(0,12); //Range to select from for Expiration Dates
+  std::uniform_int_distribution<int> money_range(301,15000); //Range to select from for amount within accounts
+  //std::uniform_int_distribution
+
   std::string full; //Full String of all fields
   std::string cc_num; //Credit Card Number
   std::string exp; //Expiration Date
+  std::string money; //Amount of money in the accounts
 
 for (int i = 0; i < 3; i ++) { //Testing for multiple random numbers
 
   exp = generate_exp(generator, months);
   cc_num = generate_cc(generator, digits);
+  money = generate_money(generator, money_range);
 
-  full = cc_num + ' ' + exp;
+  full = money + ' ' + cc_num + ' ' + exp;
 
   file << full << std::endl;
   std::cout << full << std::endl;

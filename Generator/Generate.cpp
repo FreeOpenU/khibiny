0113ed1baf0b99a110cc1dbdedcf6e9a57d6b570
Atrytone <the.atrytone@gmail.com>
2016-08-16 22:24:59 -0700

    Wrapped in class,
    Wrapped the generator in the Generator class
    
    Completed the Account Holder function
    Added a write to file function
    
    Cleaned up the H file
    Cleaned up the main file
    
    Main file showcases two ways to generate files and strings.

--- Generator/Generate.cpp
+++ Generator/Generate.cpp
@@ -1,13 +1,47 @@
 #include "Generate.h"
 
 
-std::string generate_holder(std::minstd_rand0 &generator, std::uniform_int_distribution<int> index) {
-  std::string full_name = "";
 
+Generator::Generator(std::string file) {
+  this->fileName = file;
+}
+
+void Generator::write_to_file(int iterations) {
+  //Idea: If future types of information is made for generation, could use Enums to write different types
+  std::ofstream file;
+  file.open(this->fileName, std::ios::out | std::ios::app);
+  std::string full;
+  for (int i = 0; i < iterations; i ++) {
+    full = generate_full();
+    file << full << std::endl;
+  }
+  file.close();
+}
+
+std::string Generator::generate_full() {
+    std::string full = "";
+    std::string holder = generate_holder(this->generator, this->first_name_range, this->last_name_range);
+    std::string account = generate_acc(this->generator, this->digits);
+    std::string money = generate_money(this->generator, this->money_range);
+    std::string credit_card = generate_cc(this->generator, this->digits);
+    std::string expiration = generate_exp(this->generator, this->months);
+
+    full = holder + "\n" + account + "\n" + money + "\n" + credit_card + "\n" + expiration + "\n";
+    return full;
+}
+
+
+std::string Generator::generate_holder(std::minstd_rand0 &generator, std::uniform_int_distribution<int> first_index,
+                                        std::uniform_int_distribution<int>last_index) {
+  std::string full_name = "";
+  std::string first_name = this->first_names[first_index(generator)]; //Grabs a random first name from first_name vector
+  std::string last_name = this->last_names[last_index(generator)]; //Grabs a random last name from last_name vector
+
+  full_name = first_name + " " + last_name;
   return full_name;
 }
 
-std::string generate_acc(std::minstd_rand0 &generator, std::uniform_int_distribution<int> digits) {
+std::string Generator::generate_acc(std::minstd_rand0 &generator, std::uniform_int_distribution<int> digits) {
   std::string account_num = "Account Number: ";
 
   for (int i = 0; i < 10; i++) { //The Typical bank account has 9 or 10 digits to safely hold its member count
@@ -17,7 +51,7 @@ std::string generate_acc(std::minstd_rand0 &generator, std::uniform_int_distribu
   return account_num;
 }
 
-std::string generate_money(std::minstd_rand0 & generator,
+std::string Generator::generate_money(std::minstd_rand0 & generator,
                           std::uniform_int_distribution<int> size) {
 
   std::string full_string = "";
@@ -33,8 +67,8 @@ std::string generate_money(std::minstd_rand0 & generator,
   int change_check = change(generator);
   int change_savings = change(generator);
 
-  int check_amount_int = digits(generator);
-  int savings_amount_int = digits(generator);
+  int check_amount_int = size(generator);
+  int savings_amount_int = size(generator);
 
   checking += std::to_string(check_amount_int);
   checking += ".";
@@ -50,7 +84,7 @@ std::string generate_money(std::minstd_rand0 & generator,
 
 }
 
-std::string generate_cc(std::minstd_rand0 & generator,
+std::string Generator::generate_cc(std::minstd_rand0 & generator,
                         std::uniform_int_distribution<int> digits) {
   std::string cc_num = "Credit Card Number: ";
 
@@ -65,7 +99,7 @@ std::string generate_cc(std::minstd_rand0 & generator,
 
 }
 
-std::string generate_exp(std::minstd_rand0 & generator,
+std::string Generator::generate_exp(std::minstd_rand0 & generator,
                          std::uniform_int_distribution<int> months) {
   std::string expiration = "EXP: ";
   std::string month;
--- Generator/main.cpp
+++ Generator/main.cpp
@@ -1,35 +1,20 @@
 #include "Generate.h"
-#include <fstream>
+
 
 int main () {
   std::ofstream file;
   file.open("information.txt", std::ios::out | std::ios::app);
-
-  time_t timer;
-  unsigned seed = time(&timer);
-  std::minstd_rand0 generator(seed);
-  std::uniform_int_distribution<int> digits(0,9); //Range to select from for CC numbers
-  std::uniform_int_distribution<int> months(0,12); //Range to select from for Expiration Dates
-  std::uniform_int_distribution<int> money_range(301,15000); //Range to select from for amount within accounts
-  //std::uniform_int_distribution
+  Generator account_info("ThisIsATest.txt");
 
   std::string full; //Full String of all fields
-  std::string cc_num; //Credit Card Number
-  std::string exp; //Expiration Date
-  std::string money; //Amount of money in the accounts
 
-for (int i = 0; i < 3; i ++) { //Testing for multiple random numbers
+for (int i = 0; i < 1000; i ++) {
+  full = account_info.generate_full();
+  file << full;
 
-  exp = generate_exp(generator, months);
-  cc_num = generate_cc(generator, digits);
-  money = generate_money(generator, money_range);
-
-  full = money + ' ' + cc_num + ' ' + exp;
-
-  file << full << std::endl;
-  std::cout << full << std::endl;
 }
+file.close();
+account_info.write_to_file();
 
-  file.close();
 return 0;
 }

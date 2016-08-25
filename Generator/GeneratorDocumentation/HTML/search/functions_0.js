ebbbe349a2dcf9a917ad3aa44bb8bbf7e0985e49
Atrytone <the.atrytone@gmail.com>
2016-08-25 15:57:58 -0700

    Added documentation files for respective formats.

---
+++ Generator/GeneratorDocumentation/HTML/search/functions_0.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['generate_5facc',['generate_acc',['../class_generator.html#a7d63f70a9e8ef7c2f99498318bcf558d',1,'Generator']]],
+  ['generate_5fcc',['generate_cc',['../class_generator.html#a16404b548c308334e1502619449d4f23',1,'Generator']]],
+  ['generate_5fexp',['generate_exp',['../class_generator.html#a443048cb6d0a5f29867133c13332a268',1,'Generator']]],
+  ['generate_5ffull',['generate_full',['../class_generator.html#a5fd55ab53733cc8ef626fc1d820bd5bd',1,'Generator']]],
+  ['generate_5fholder',['generate_holder',['../class_generator.html#a9ad03b3204c0809c70c0c496c84a4cb0',1,'Generator']]],
+  ['generate_5fmoney',['generate_money',['../class_generator.html#ac2682698e6e1ef7cc7f4b2c7dd11482b',1,'Generator']]],
+  ['generator',['Generator',['../class_generator.html#ae2d35b67b9814dd4600c10be5e397811',1,'Generator']]]
+];
---
+++ Generator/GeneratorDocumentation/HTML/search/functions_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['write_5fto_5ffile',['write_to_file',['../class_generator.html#a097fbf1a97fb6dd2f8c1f7f4423dcb79',1,'Generator']]]
+];
